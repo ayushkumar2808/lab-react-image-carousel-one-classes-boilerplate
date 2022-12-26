83faabc65721cff9a78e6387186dd46081e5c821
Ayush Kumar <115335623+Ayush-28-08@users.noreply.github.com>
2022-12-26 10:08:08 +0530

    Done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,6 +7,54 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
+    constructor(){
+        super()
+        this.state={
+            image:0
+        }
+    }
+    imgBack(){
+        if(this.state.image>0){
+            this.setState({
+                image:this.state.image-1
+            })
+        }
+        else if(this.state.image===0){
+            this.setState({
+                image:2
+            })
+        }
+    }
+    imgForward(){
+        if(this.state.image<2){
+            this.setState({
+                image:this.state.image+1
+            })
+        }
+        else if(this.state.image===2){
+            this.setState({
+                image:0
+            })
+        }
+    }
+
+
+
+    render(){
+        return(
+            <>
+            <div id="display">
+                <div className="border" onClick={()=>this.imgBack()}><ArrowBackIosIcon /></div>
+                <div id="imgdiv">
+                <h1 id="title">{images[this.state.image].title}</h1>
+                <img id="image" src={images[this.state.image].img} alt="" />
+                <h1 id="subtitle">{images[this.state.image].subtitle}</h1>
+                </div>
+                <div className="border" onClick={()=>this.imgBack()}><ArrowForwardIosIcon /></div>
+            </div>
+            </>
+        )
+    }
   
 }
 
